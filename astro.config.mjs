f5de2b9475a37fa63085679d6ac7c717918bf891
jen444x <106173914+jen444x@users.noreply.github.com>
2025-11-18 16:12:29 -0800

    Update portfolio website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.jennifermiranda.com",
   trailingSlash: "never",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
